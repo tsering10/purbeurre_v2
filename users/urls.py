0f993ee63ca8f87927c6569f213cc9036fcbeb9d
tashi tsering <tashi.cse@gmail.com>
2021-09-03 12:59:23 +0200

    users urls formated

--- users/urls.py
+++ users/urls.py
@@ -12,20 +12,6 @@ urlpatterns = [
     path('saved/', views.saved, name='saved'),
     path("password_reset",auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html",),name="password_reset",),
     path("password-reset/done",auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),name="password_reset_done",),
-    
-    path(
-        "password-reset-confirm/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name="registration/password_reset_confirm.html"
-        ),
-        name="password_reset_confirm",
-    ),
-    
-    path(
-        "password_reset/complete",
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name="registration/password_reset_complete.html",
-        ),
-        name="password_reset_complete",
-    ),
+    path("password-reset-confirm/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"),name="password_reset_confirm",),
+    path("password_reset/complete", auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html",),name="password_reset_complete",),
 ]
